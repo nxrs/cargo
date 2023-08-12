66a38ea571b2ade8fc59e727dbea25f7454d9246
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2023-08-12 18:15:50 +0200

    fix: ENOBUFS error in large projects (#29)

--- packages/cargo/src/graph/index.ts
+++ packages/cargo/src/graph/index.ts
@@ -13,6 +13,7 @@ export function processProjectGraph(
 ): ProjectGraph {
 	let metadata = cp.execSync("cargo metadata --format-version=1", {
 		encoding: "utf8",
+		maxBuffer: 1024 * 1024 * 1024 * 32,
 	});
 	let { packages, workspace_members } = JSON.parse(metadata);
 	let builder = new ProjectGraphBuilder(graph);
